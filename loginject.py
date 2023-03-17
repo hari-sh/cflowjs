edd54bb8aa18c0266e342be2043a70697e302eca
hari-sh <harikalyanraman@gmail.com>
2023-03-18 02:15:14 +0530

    basic.c issue there

--- loginject.py
+++ loginject.py
@@ -141,5 +141,5 @@ pattern = "*.c"
 for path, subdirs, files in os.walk(root):
     for name in files:
         if fnmatch(name, pattern):
-            print(os.path.join(path, name))
+            procfile(os.path.join(path, name))
 
