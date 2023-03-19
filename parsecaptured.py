d1732db8cc94ec191f3aa7350ab6ff27297ba278
hari-sh <harikalyanraman@gmail.com>
2023-03-19 13:30:30 +0530

    update tracker

--- parsecaptured.py
+++ parsecaptured.py
@@ -1,5 +1,16 @@
 import os
 
+
+def updatetracker(name, incdec):
+    if name in trackcount:
+        trackcount[name] += incdec
+    else:
+        trackcount[name] = incdec
+    
+
+
+trackcount = {}
+
 with open("src/obtlog.txt", 'r') as infile,  open("temp.txt", 'w') as outfile:
     inden = 0
     for line in infile:
@@ -8,7 +19,14 @@ with open("src/obtlog.txt", 'r') as infile,  open("temp.txt", 'w') as outfile:
             name = splt[0]
             mode = splt[1]
             if mode == 'entry':
+                updatetracker(name, 1)
                 outfile.write(name.rjust(inden) + os.linesep)
                 inden = inden + 4
             elif mode == 'exit':
+                updatetracker(name, -1)
                 inden = inden - 4
+
+for key in trackcount:
+    val = trackcount[key]
+    if val != 0:
+        print(key +" : " + str(val))
